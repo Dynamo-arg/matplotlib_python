acc5f51862961bf7dbb2dc5981fbe1b7934edb00
Dynamo-arg <72266387+Dynamo-arg@users.noreply.github.com>
2021-02-09 09:24:53 -0300

    Add files via upload

--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -1,197 +1,281 @@
 #!/usr/bin/env python
 '''
-Numpy [Python]
+Matplotlib [Python]
 Ejercicios de profundización
 ---------------------------
 Autor: Inove Coding School
 Version: 1.1
-
 Descripcion:
-Programa creado para que practiquen los conocimietos
-adquiridos durante la semana
+Programa creado para poner a prueba los conocimientos
+adquiridos durante la clase
 '''
 
 __author__ = "Sebastian Volpe"
 __email__ = "alumnos@inove.com.ar"
 __version__ = "1.1"
 
-import numpy as np
-import random
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.axes
+import matplotlib.gridspec as gridspec
+import mplcursors  # [Opcional cursores]
+
 
 def ej1():
-    print('Comenzamos a divertirnos!')
+    # Line Plot
+    # Se desea graficar tres funciones en una misma figura
+    # en tres gráficos (axes) distintos. Para el siguiente
+    # intervalor de valores de x:
+    x = list(range(-10, 11, 1))
 
-    '''
-    Empecemos a jugar con las listas y su métodos, el objetivo
-    es realizar el código lo más simple, ordenado y limpio posible,
-    no utilizar bucles donde no haga falta, no "re inventar" una función
-    que ya dispongamos de Python. El objetivo es:
+    # Realizar tres gráficos que representen
+    # y1 = x^2 (X al cuadrado)
+    # y2 = x^3 (X al cubo)
+    # y3 = x^4 (X a la cuarta)
+    # Utilizar comprension de listas para generar
+    # y1, y2 e y3 basado en los valores de x
 
-    1) Generar una lista 3 numéros aleatorios con random (pueden repetirse),
-       que estén comprendidos entre 1 y 10 inclusive
-       (NOTA: utilizar comprension de listas a pesar de poder hacerlo
-        con un método de la librería random)
+    y1 = [i**2 for i in x]
+    y2 = [i**3 for i in x]
+    y3 = [i**4 for i in x]
 
-    2) Luego de generar la lista sumar los números y ver si el resultado
-       de la suma es menor o igual a 21
-       a) Si el número es menor o igual a 21 se imprime en pantalla
-          la suma y los números recoletados
-       b) Si el número es mayor a 21 se debe tirar la lista y volver
-          a generar una nueva, repetir este proceso hasta cumplir la
-        condicion "a"
+    # Esos tres gráficos deben estar colocados
+    # en la diposición de 3 filas y 1 columna:
+    # ------
+    # graf1
+    # ------
+    # graf2
+    # ------
+    # graf3
+    # ------
+    # Utilizar add_subplot para lograr este efecto
+    # de "3 filas" "1 columna" de gráficos
 
-    Realizar este proceso iterativo hasta cumplir el objetivo
-    '''
-    suma = 100
-    while suma >= 21:
-        lista_random = [random.randint(1, 10) for x in range(3)]
-        suma = np.sum(lista_random)
-    print("Numeros recolectados", lista_random, "suma total", suma)
- 
+    # Se debe colocar en la leyenda la función que representa
+    # cada gráfico
+
+    # Cada gráfico realizarlo con un color distinto
+    # a su elección
+
+    gs = gridspec.GridSpec(3, 1)    
+    fig = plt.figure("Ejercicio 1")
+    ax1 = fig.add_subplot(gs[0, 0])  
+    ax2 = fig.add_subplot(gs[1, 0])  
+    ax3 = fig.add_subplot(gs[2, 0])  
+
+    ax1.plot(y1, color='darkgreen', label='(X al cuadrado)')
+    ax1.set_facecolor('whitesmoke')
+    ax1.legend()
+    ax1.grid('solid')
+
+    ax2.plot(y2, color='r', label='(X al cubo)')
+    ax2.set_facecolor('grey')
+    ax2.legend()
+    ax2.grid('solid')
+
+    ax3.plot(y3, color='blue', label='(X a la cuarta)')
+    ax3.set_facecolor('white')
+    ax3.legend()
+    ax3.grid('solid')
+
+
+    plt.show()
 
 
 def ej2():
-    print('Comenzamos a ponernos serios!')
+    # Scatter Plot
+    # Se desea graficar dos funciones en una misma figura
+    # en dos gráficos (axes) distintos. Para el siguiente
+    # intervalor de valores de x:
+    x = np.arange(0, 4*np.pi, 0.1)
 
-    '''
-    Dado una lista de nombres de personas "nombres" se desea
-    obtener una nueva lista filtrada que llamaremos "nombres_filtrados"
-    La lista se debe filtrar por comprensión de listas utilizando la
-    lista "padron" como parámetro.
-    La lista filtrada sodo deberá tener aquellos nombres que empiecen
-    con alguna de las letras aceptadas en el "padron".
-    '''
+    # Realizar dos gráficos que representen
+    # y1 = sin(x)
+    # y2 = cos(x)
+    # Utilizar los métodos de Numpy para calcular
+    # "y1" y "y2" basado en los valores de x
 
-    padron = ['A', 'E', 'J', 'T']
+    y1 = [np.sin(i) for i in x]
+    y2 = [np.cos(i) for i in x]
+    
+    # Esos dos gráficos deben estar colocados
+    # en la diposición de 1 fila y 2 columnas:
+    # ------
+    #  graf1 | graf2
+    # ------
+    # Utilizar add_subplot para lograr este efecto
+    # de "1 fila" "2 columnas" de gráficos
 
-    nombres = ['Tamara', 'Marcelo', 'Martin', 'Juan', 'Alberto', 'Exequiel',
-               'Alejandro', 'Leonel', 'Antonio', 'Omar', 'Antonia', 'Amalia',
-               'Daniela', 'Sofia', 'Celeste', 'Ramon', 'Jorgelina', 'Anabela']
+    # Se debe colocar en la leyenda la función que representa
+    # cada gráfico
 
-    nombres_filtrados = [x for x in nombres if x[0] in padron] 
-    print(nombres_filtrados)
+    # Cada gráfico realizarlo con un mark distinto
+    # a su elección.
+
+    gs = gridspec.GridSpec(1, 2)    
+    fig = plt.figure("Ejercicio 2")
+    ax1 = fig.add_subplot(gs[0, 0])  
+    ax2 = fig.add_subplot(gs[0, 1])  
+
+    ax1.plot(x,y1, color='darkgreen', marker='+', label='y1 = sin(x)')
+    ax1.set_facecolor('whitesmoke')
+    ax1.legend()
+    ax1.grid('solid')
+
+    ax2.plot(x,y2, color='r', marker='1',label='y1 = cos(x)')
+    ax2.set_facecolor('white')
+    ax2.legend()
+    ax2.grid('solid')
+
+    plt.show()
 
 
-    # Se espera obtener:
-    # ['Tamara', 'Juan', 'Alberto'......]
 
 
 def ej3():
-    print("Un poco de Numpy!")
-    # Ejercicio de funciones
-    # Se desea calcular los valores de salida de
-    # una función senoidal, dado "X" como el conjunto
-    # de valores que deben someter a la función "sin"
+    # Bar Plot
+    # Generar un gráfico de barras simple a partir
+    # de la siguiente información:
 
-    # Conjunto de valores "X" en un array
-    x = np.arange(0, 2*np.pi, 0.1)
+    lenguajes = ['Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp']
+    performance = [10, 8, 6, 4, 2, 1]
 
-    # Utilizar la función np.sin para someter cada valor de "X",
-    # obtenga el array "y_nump" que tenga los resultados
-    # NO utilizar comprensión de listas, solo utilice la
-    # funcion de numpy "np.sin"
+    # Realizar un gráfico de barras en donde se pueda ver el uso
+    # de cada lenguaje, se debe utilizar los labels "lenguajes"
+    # como valor del eje X
 
-    y_nump = np.sin(x)
+    # Se debe colocar título al gráfico.
+    # Se debe cambiar la grilla y el fondo a su elección.
 
-    # Conjunto de valores "X" en una lista
-    x = list(np.arange(0, 2*np.pi, 0.1))
+    width = 0.2  # Tamaño de la barra
+    fig = plt.figure("Ejercicio 3")
+    fig.suptitle('Lenguajes', fontsize=16)
+    ax = fig.add_subplot()
 
-    # Utilizar comprensión de listas para obtener la lista
-    # "y_list" que tenga todos los valores obtenidos como resultado
-    # de someter cada valor de "X" a la función math.sin
-
-    y_list = [math.sin(a) for a in x]
-    print(y_list)
-
-
-    # Este es un ejemplo práctico de cuando es útil usar numpy,
-    # basicamente siempre que deseen utilizar una función matemática
-    # que esté definida en numpy NO necesitaran un bucle o comprensión
-    # de listas para obtener el resultado de un monton de datos a la vez.
+    ax.bar(lenguajes, performance, 
+            width=0.5, color=['red', 'blue', 'purple', 'green', 'grey', 'gold'],
+            label='Lenguajes')
+    ax.set_facecolor('white')
+    ax.legend()
+    ax.set_xticks(lenguajes)
+    ax.set_xticklabels(lenguajes)
+    plt.show()
 
 
 def ej4():
-    print("Acercamiento al uso de datos relacionales")
-    # Transformar variable numéricas en categóricas
-    # Se dispone del siguiente diccionario que traduce el número ID
-    # de un producto en su nombre, por ejemplo:
-    # NOTA: Esta información bien podría ser una tabla SQL: id | producto
-    producto = {
-                556070: 'Auto',
-                704060: 'Moto',
-                42135: 'Celular',
-                1264: 'Bicicleta',
-                905045: 'Computadora',
-                }
+    # Pie Plot
+    # Se desea realizar un gráfico de torta con la siguiente
+    # información acerca del % de uso de lenguajes en nuevos
+    # programadores
+    uso_lenguajes = {'Python': 29.9, 'Javascript': 19.1,
+                     'Go': 16.2, 'Java': 10.5, 'C++': 10.2,
+                     'C#': 8.2, 'C': 5.9
+                     }
 
-    lista_compra_id = [556070, 905045, 42135, 5674, 704060, 1264, 42135, 3654]
 
-    # Crear una nueva lista "lista_compra_productos" que transforme la lista
-    # de realizada por "ID" de producto en lista por "nombre" producto
-    # Iterar sobre la lista "lista_compra_id" para generar la nueva
-    # En cada iteración acceder al diccionario para traducir el ID.
-    # NOTA: Tener en cuenta que puede haber códigos (IDs) que
-    # no esten registrados en el sistema, en esos casos se debe
-    # almacenar en la lista la palabra 'NaN', para ello puede hacer uso
-    # de condicionales PERO recomendamos leer atentamente el método "get"
-    # de diccionarios que tiene un parametro configurable respecto
-    # que sucede sino encuentra la "key" en el diccionario
-    #nombres_filtrados = [x for x in nombres if x[0] in padron] 
-    lista_compra_productos = [{x:producto.get(x,"NaN")} for x in lista_compra_id]
-    print(lista_compra_productos)
+    # El gráfico debe tener usar como label las keys del diccionario,
+    # debe usar como datos los values del diccionario
+    # Se desea resaltar (explode) el dato de Python
+    # Se desea mostrar en el gráfico los porcentajes de c/u
+    # Se debe colocar un título al gráfico
+
+    fig = plt.figure()
+    fig.suptitle('Lenjuages Compraracion', fontsize=16)
+    ax = fig.add_subplot()
+
+    explode = (0.1, 0, 0 ,0 ,0 ,0 ,0)  # solo resaltar el consumo de carne
+
+    ax.pie(uso_lenguajes.values(), labels=uso_lenguajes.keys(),
+           explode=explode, autopct='%1.1f%%', shadow=True, startangle=90
+           )
+    ax.axis('equal')
+    plt.show()
+
 
 
 
 def ej5():
-    print("Ahora sí! buena suerte :)")
+    # Uso de múltiples líneas en un mismo gráfico (axes)
+    # En el siguiente ejemplo generaremos una señal senoidal
+    # haciendo uso solamente de comprension de listas
+    step = 0.1
+    sample_size = 100
+    signal = [{'X': i*step, 'Y': math.sin(i*step)} for i in range(sample_size)]
 
-    '''
-    Black jack! [o algo parecido :)]
+    # Se generó una lista de diccionarios con dos columnas "X" e "Y"
+    # que corresponden a los valores de nuestra señal senoidal.
+    # Se pide usar comprensión de listas para generar las dos listas
+    # por separado de los valoresde "X" e "Y" para poder utilizar
+    # el line plot y observar la señal
 
-    El objetivo es realizar una aproximación al juego de black jack,
-    el objetivo es generar una lista de 2 números aleatorios
-    entre 1 al 10 inclusive, y mostrar los "números" al usuario.
-    El usuario debe indicar al sistema si desea sacar más
-    números para sumarlo a la lista o no sacar más
-    A medida que el usuario vaya sacando números aleatorios que se suman
-    a su lista se debe ir mostrando en pantalla la suma total
-    de los números hasta el momento.
-    Cuando el usuario no desee sacar más números o cuando el usuario
-    haya superado los 21 (como la suma de todos) se termina la jugada
-    y se presenta el resultado en pantalla
-
-    BONUS Track: Realizar las modificaciones necesarias para que jueguen
-    dos jugadores y compitan para ver quien sacá la suma de números
-    más cercanos a 21 sin pasarse!
-    '''
-    print("BLACK JACK !!")
-    cartas = [random.randint(1, 10) for x in range(2)]
-    print("Cartas repartidas:", cartas)
-    while sum(cartas) < 21:
-        nueva_carta = input("Desea pedir nueva carta (S/N)")
-        if nueva_carta == "s":
-            cartas.append(random.randint(1, 10))
-            print("TOTAL CONTEO CARTAS:",sum(cartas))
-        else:
-            print("Juego terminado TOTAL:",sum(cartas))
-            break
-    if sum(cartas) > 22:
-        print("JUEGO TERMINADO PERDIO TOTAL:", sum(cartas))
-    elif sum(cartas) == 21:
-        print("BLACK JACK!!!!!!")
-    else:
-        print("JUEGO TERMINADO puntaje:", sum(cartas))
-
+    signal_x =  [x.get('X', '') for x in signal]
     
-    
+    signal_y = [x.get('Y', '') for x in signal]
 
+    # plot(signal_x, signal_y)
+
+    gs = gridspec.GridSpec(1, 2)     
+    fig = plt.figure()
+    ax1 = fig.add_subplot(gs[0, 0]) 
+    ax2 = fig.add_subplot(gs[0, 1])  
+
+    ax1.plot(signal_x, color='darkgreen', label='Señal X')
+    ax1.set_facecolor('whitesmoke')
+    ax1.legend()
+    ax1.grid('solid')
+
+    ax2.plot(signal_y, color='darkblue', label='Señal Y')
+    ax2.set_facecolor('whitesmoke')
+    ax2.legend()
+    ax2.grid('solid')
+    plt.show()
+
+
+
+    # Ahora que han visto la señal senoidal en su gráfico, se desea
+    # que generen otras dos listas de "X" e "Y" pero filtradas por
+    # el valor de "Y". Solamente se debe completar la lista
+    # con aquellos valores de "Y" cuyo valor absoluto (abs)
+    # supere 0.7
+
+    filter_signal_x = [data for data in signal_x if data > abs(0.7)]
+    filter_signal_y = [data for data in signal_y if data > abs(0.7)]
+    
+    # Graficar juntas ambos conjuntos de listas y observar
+    # el resultado. Graficar filter como scatter plot
+
+    # plot(signal_x, signal_y)
+    # scatter(filter_signal_x, filter_signal_y)
+
+    gs = gridspec.GridSpec(1, 2)     
+    fig = plt.figure()
+    ax1 = fig.add_subplot(gs[0, 0])  
+    ax2 = fig.add_subplot(gs[0, 1])  
+    fig.suptitle('Filtrados por Valor Absoluto mas 0.7"', fontsize=14)
+
+
+    ax1.scatter([i for i in filter_signal_y],filter_signal_y ,c='darkred', marker='^',  label='Filtrado Y')
+    ax1.legend()
+    ax1.grid()
+    ax1.set_facecolor('whitesmoke')
+
+    ax2.scatter([i for i in filter_signal_x],filter_signal_x ,c='black', marker='+',  label='Filtrado X')
+    ax2.legend()
+    ax2.grid()
+    ax2.set_facecolor('whitesmoke')
+
+
+    plt.show()
+
+    # Pueden ver el concepto y la utilidad de
+    # realizar un gráfico encima de otro para filtrar datos?
 
 
 if __name__ == '__main__':
-    print("Ejercicios de práctica")
-    # ej1()
-    # ej2()
-    # ej3()
-    # ej4()
+    print("Bienvenidos a otra clase de Inove con Python")
+    ej1()
+    ej2()
+    ej3()
+    ej4()
     ej5()
